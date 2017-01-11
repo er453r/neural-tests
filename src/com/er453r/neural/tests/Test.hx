0d4d24695517b19d9cf1a7f4d60fa8c5ef0f3925
Marcin Kotz <marcin.kotz@ives-system.com>
2017-01-11 15:31:08 +0100

    first display test

--- src/com/er453r/neural/tests/Test.hx
+++ src/com/er453r/neural/tests/Test.hx
@@ -1,8 +1,46 @@
 package com.er453r.neural.tests;
+
+import js.html.Uint8ClampedArray;
+import js.html.ImageData;
+import js.html.CanvasRenderingContext2D;
+import js.Browser;
+import js.html.CanvasElement;
+
+import haxe.ds.Vector;
+
 class Test {
 	public static function main() {
-		trace("lol");
+		Browser.document.addEventListener("DOMContentLoaded", init);
+	}
 
-		new Neuron();
+	private static function init(){
+		var size = 128;
+
+		var neurons:Vector<Neuron> = new Vector<Neuron>(size * size);
+
+		for(n in 0...neurons.length){
+			neurons[n] = new Neuron();
+		}
+
+		var canvas:CanvasElement = Browser.document.createCanvasElement();
+		canvas.width = size;
+		canvas.height = size;
+
+		Browser.document.body.appendChild(canvas);
+
+		var context:CanvasRenderingContext2D = canvas.getContext2d();
+
+		var image:ImageData = context.getImageData(0, 0, size, size);
+
+		var pixels:Uint8ClampedArray = image.data;
+
+		for(n in 0...size*size){
+			pixels[4*n + 0] = Math.round(255 * neurons[n].value); // Red value
+			pixels[4*n + 1] = Math.round(255 * neurons[n].value); // Green value
+			pixels[4*n + 2] = Math.round(255 * neurons[n].value); // Blue value
+			pixels[4*n + 3] = 255; // Alpha value
+		}
+
+		context.putImageData(image, 0, 0);
 	}
 }
