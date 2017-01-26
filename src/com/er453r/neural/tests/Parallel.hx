f2cae719b6ddd56913c818e4b416f69794b5f250
Marcin Kotz <marcin.kotz@ives-system.com>
2017-01-26 11:38:11 +0100

    first approach to macro worker building

--- src/com/er453r/neural/tests/Parallel.hx
+++ src/com/er453r/neural/tests/Parallel.hx
@@ -1,18 +1,20 @@
 package com.er453r.neural.tests;
 
-import js.Browser;
-import js.html.Worker;
+import haxe.macro.ExprTools;
+import haxe.macro.Expr.ExprOf;
+import haxe.macro.Expr;
+
+/*import js.html.Worker;
 import js.Promise;
 import js.html.URL;
-import js.html.Blob;
-import js.Browser;
+import js.html.Blob;*/
 
 import haxe.ds.Vector;
 
 class Parallel {
 	public function new() {
 	}
-
+/*
 	private static function createWorker(n:UInt, url:String):Promise<Int>{
 		return new Promise<Int>(function(resolve:Int->Void, reject:Dynamic->Void){
 			var worker = new Worker(url);
@@ -40,26 +42,37 @@ class Parallel {
 
 		untyped __js__('onmessage = messageHandler');
 	}
+*/
+	public static macro function forEach<T>(data:ExprOf<Vector<T>>, job:ExprOf<T->Void>):Expr{
+		trace(job.pos);
+		trace(ExprTools.toString(job));
 
-	public static function forEach<T>(data:Vector<T>, job:T->Void, parallel:Bool = true){
-		if(parallel){
-			var workerBody:String = "";//untyped __js__('job.toString()');
+		/*if(parallel){
+			var jobBody:String = untyped __js__('job.toString()') + "\n\n";
 
-			workerBody += "\n\n(" + untyped __js__('com_er453r_neural_tests_Parallel.workerBase.toString()') + ")()";
+			jobBody = StringTools.replace(jobBody, "function (", "function jon(");
+
+			var workerBody:String  = jobBody + '(' + untyped __js__('com_er453r_neural_tests_Parallel.workerBase.toString()') + ')()';
 
 			trace(workerBody);
 
-			var blob:Blob = new Blob(['(', workerBody, ')()'], {type:'text/javascript'});
+			var blob:Blob = new Blob([workerBody], {type:'text/javascript'});
 			var url:String = URL.createObjectURL(blob);
 
 			var workers:Array<Promise<Int>> = [];
 
-			for(n in 0...5)
-				workers.push(createWorker(n, url));
+			for(n in 0...5){
+				//workers.push(createWorker(n, url));
+
+				var worker = new Worker(url);
+			}
+
 		}
 		else{
 			for(object in data)
 				job(object);
-		}
+		}*/
+
+		return macro {};
 	}
 }
