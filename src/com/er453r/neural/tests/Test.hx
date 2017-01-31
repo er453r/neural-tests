af18012392a1d1b9103880a3710f203a9ce5eda4
Marcin Kotz <marcin.kotz@ives-system.com>
2017-01-31 11:39:47 +0100

    really nice artistic effect :)

--- src/com/er453r/neural/tests/Test.hx
+++ src/com/er453r/neural/tests/Test.hx
@@ -24,8 +24,8 @@ class Test{
 
 	private var network:Network;
 
-	private var width:Int = 1 * 64;
-	private var height:Int = 1 * 64;
+	private var width:Int = 2 * 64;
+	private var height:Int = 2 * 64;
 
 	public static function main(){
 		new Test();
