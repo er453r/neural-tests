e8de1464d62697088ead1a56bc46876e787b7a33
er453r <er453r@gmail.com>
2017-02-22 23:13:16 +0100

    learing path works

--- src/com/er453r/neural/tests/Test.hx
+++ src/com/er453r/neural/tests/Test.hx
@@ -16,7 +16,6 @@ import com.er453r.neural.nets.Network;
 class Test{
 	private var output:Image;
 	private var learning:Image;
-	private var learningMask:Image;
 	private var plot:Plot;
 
 	private var fps:FPS = new FPS();
@@ -24,8 +23,8 @@ class Test{
 
 	private var network:Network;
 
-	private var width:Int = 1 * 9;
-	private var height:Int = 1 * 7;
+	private var width:Int = 1 * 32;
+	private var height:Int = 1 * 32;
 
 	public static function main(){
 		new Test();
@@ -39,7 +38,6 @@ class Test{
 		stats = Browser.document.getElementById("fps");
 		output = new Image(width, height);
 		learning = new Image(width, height, new Viridis());
-		learningMask = new Image(width, height, new Viridis());
 		network = new FlatNet(width, height, 1);
 		plot = new Plot(width, height);
 
@@ -62,33 +60,27 @@ class Test{
 	private function loop(){
 		network.update();
 
-		if(skip++ % 4 == 0){
-			output.generic(network.getNeurons(), function(neuron:Neuron):Float{
-				return neuron.value;
-			});
+		output.generic(network.getNeurons(), function(neuron:Neuron):Float{
+			return neuron.value;
+		});
 
-			learningMask.generic(network.getNeurons(), function(neuron:Neuron):Float{
-				return neuron.value > 0.000001 ? 1 : 0;
-			});
+		learning.generic(network.getNeurons(), function(neuron:Neuron):Float{
+			return neuron.learning;
+		});
 
-			learning.generic(network.getNeurons(), function(neuron:Neuron):Float{
-				return neuron.learning;
-			});
+		var outputIndex:UInt = Std.int(height / 2) * width + Std.int(3 * width / 4);
 
-			var outputIndex:UInt = Std.int(height / 2) * width + Std.int(3 * width / 4);
+		outs.push(network.getNeurons().get(outputIndex).value);
 
-			outs.push(network.getNeurons().get(outputIndex).value);
+		while(outs.length > 100)
+			outs.shift();
 
-			while(outs.length > 100)
-				outs.shift();
+		plot.floats(outs);
 
-			plot.floats(outs);
+		stats.innerHTML = 'FPS ${fps.update()}';
 
-			stats.innerHTML = 'FPS ${fps.update()}';
-		}
-		else
-			fps.update();
+		fps.update();
 
-		Timer.delay(loop, 1000);
+		Timer.delay(loop, 10);
 	}
 }
